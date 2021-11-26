45d5674417108ee34e7400b03aab088b2faacfc3
matievisthekat <matievisthekat@gmail.com>
2021-11-26 22:59:09 +0200

    add arena crate

---
+++ arena/src/lib.rs
@@ -0,0 +1,47 @@
+use std::ops::Index;
+use std::marker::PhantomData;
+
+#[derive(Debug)]
+pub struct Idx<T> {
+  raw: u32,
+  _phantom: PhantomData<fn() -> T>,
+}
+
+#[derive(Debug)]
+pub struct Arena<T> {
+  data: Vec<T>,
+}
+
+impl<T> Default for Arena<T> {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+
+impl<T> Index<Idx<T>> for Arena<T> {
+  type Output = T;
+
+  fn index(&self, idx: Idx<T>) -> &Self::Output {
+    &self.data[idx.raw as usize]
+  }
+}
+
+impl<T> Arena<T> {
+  pub fn new() -> Self {
+    Self { data: Vec::new() }
+  }
+
+  pub fn alloc(&mut self, t: T) -> Idx<T> {
+    let idx = self.next_idx();
+    self.data.push(t);
+
+    idx
+  }
+
+  fn next_idx(&self) -> Idx<T> {
+    Idx {
+      raw: self.data.len() as u32,
+      _phantom: PhantomData,
+    }
+  }
+}
