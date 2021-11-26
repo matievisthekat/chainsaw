d3e1bb964ee16dfa8bb7bae0da6f7cc6c0bd6ce7
matievisthekat <matievisthekat@gmail.com>
2021-11-26 12:30:44 +0200

    update version

---
+++ syntax/src/lib.rs
@@ -0,0 +1,75 @@
+use lexer::TokenKind;
+use num_derive::{FromPrimitive, ToPrimitive};
+use num_traits::{FromPrimitive, ToPrimitive};
+
+#[derive(Debug, Copy, Clone, PartialEq, FromPrimitive, ToPrimitive)]
+pub(crate) enum SyntaxKind {
+  Whitespace,
+  FuncKw,
+  SetKw,
+  Identifier,
+  Number,
+  Plus,
+  Minus,
+  Asterisk,
+  Slash,
+  Equals,
+  LParen,
+  RParen,
+  LBrace,
+  RBrace,
+  Comment,
+  Error,
+  Root,
+  InfixExpr,
+  Literal,
+  ParenExpr,
+  PrefixExpr,
+  VariableRef,
+}
+
+impl SyntaxKind {
+  pub(crate) fn is_trivia(self) -> bool {
+    matches!(self, Self::Whitespace | Self::Comment)
+  }
+}
+
+impl From<TokenKind> for SyntaxKind {
+  fn from(token_kind: TokenKind) -> Self {
+    match token_kind {
+      TokenKind::Whitespace => Self::Whitespace,
+      TokenKind::FuncKw => Self::FuncKw,
+      TokenKind::SetKw => Self::SetKw,
+      TokenKind::Identifier => Self::Identifier,
+      TokenKind::Number => Self::Number,
+      TokenKind::Plus => Self::Plus,
+      TokenKind::Minus => Self::Minus,
+      TokenKind::Asterisk => Self::Asterisk,
+      TokenKind::Slash => Self::Slash,
+      TokenKind::Equals => Self::Equals,
+      TokenKind::LParen => Self::LParen,
+      TokenKind::RParen => Self::RParen,
+      TokenKind::LBrace => Self::LBrace,
+      TokenKind::RBrace => Self::RBrace,
+      TokenKind::Comment => Self::Comment,
+      TokenKind::Error => Self::Error,
+    }
+  }
+}
+
+pub(crate) type SyntaxNode = rowan::SyntaxNode<MonkeLanguage>;
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub(crate) enum MonkeLanguage {}
+
+impl rowan::Language for MonkeLanguage {
+  type Kind = SyntaxKind;
+
+  fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+    Self::Kind::from_u16(raw.0).unwrap()
+  }
+
+  fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+    rowan::SyntaxKind(kind.to_u16().unwrap())
+  }
+}
