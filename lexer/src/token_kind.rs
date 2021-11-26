0a1449b07f61d3a85e2b539c0f33eeb7a0e745d9
matievisthekat <matievisthekat@gmail.com>
2021-11-26 17:57:00 +0200

    add ranges to lexer

--- lexer/src/token_kind.rs
+++ lexer/src/token_kind.rs
@@ -1,33 +1,5 @@
 use logos::Logos;
-
-pub struct Lexer<'a> {
-  inner: logos::Lexer<'a, TokenKind>,
-}
-
-impl<'a> Lexer<'a> {
-  pub fn new(input: &'a str) -> Self {
-    Self {
-      inner: TokenKind::lexer(input),
-    }
-  }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-  type Item = Token<'a>;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    let kind = self.inner.next()?;
-    let text = self.inner.slice();
-
-    Some(Self::Item { kind, text })
-  }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Token<'a> {
-  pub kind: TokenKind,
-  pub text: &'a str,
-}
+use text_size::TextRange;
 
 #[derive(Debug, Copy, Clone, PartialEq, Logos)]
 pub enum TokenKind {
@@ -83,11 +55,14 @@ pub enum TokenKind {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::{Lexer, Token};
+  use crate::Lexer;
 
   fn check(input: &str, kind: TokenKind) {
     let mut lexer = Lexer::new(input);
-    assert_eq!(lexer.next(), Some(Token { kind, text: input }));
+
+    let token = lexer.next().unwrap();
+    assert_eq!(token.kind, kind);
+    assert_eq!(token.text, input);
   }
 
   #[test]
