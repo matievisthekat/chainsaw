013dbd52ec2b24fa1e4ebffdefd45ca560a2e982
matievisthekat <matievisthekat@gmail.com>
2021-11-26 10:40:34 +0200

    eat whitespace

--- monke-lib/src/parser/mod.rs
+++ monke-lib/src/parser/mod.rs
@@ -59,7 +59,12 @@ impl<'l, 'input> Parser<'l, 'input> {
     self.events.len()
   }
 
-  fn peek(&self) -> Option<SyntaxKind> {
+  fn peek(&mut self) -> Option<SyntaxKind> {
+    self.eat_whitespace();
+    self.peek_raw()
+  }
+
+  fn peek_raw(&self) -> Option<SyntaxKind> {
     self
       .lexemes
       .get(self.cursor)
@@ -67,6 +72,8 @@ impl<'l, 'input> Parser<'l, 'input> {
   }
 
   fn bump(&mut self) {
+    self.eat_whitespace();
+
     let Lexeme { kind, text } = self.lexemes[self.cursor];
 
     self.cursor += 1;
@@ -74,6 +81,12 @@ impl<'l, 'input> Parser<'l, 'input> {
       kind,
       text: text.into(),
     });
+  }
+
+  fn eat_whitespace(&mut self) {
+    while self.peek_raw() == Some(SyntaxKind::Whitespace) {
+      self.bump();
+    }
   }
 }
 
@@ -105,5 +118,15 @@ mod tests {
   #[test]
   fn parse_nothing() {
     check("", expect![[r#"Root@0..0"#]]);
+  }
+
+  #[test]
+  fn parse_whitespace() {
+    check(
+      "   ",
+      expect![[r#"
+Root@0..3
+  Whitespace@0..3 "   ""#]],
+    );
   }
 }
