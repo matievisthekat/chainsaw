1f8d383ece64f39ee064bdd2380648b318991bbc
matievisthekat <matievisthekat@gmail.com>
2021-11-26 09:08:28 +0200

    update crate names

---
+++ monke_cli/src/main.rs
@@ -0,0 +1,20 @@
+use monke_lib::parser::Parser;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut input = String::new();
+
+    loop {
+        write!(stdout, "→ ")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+
+        let parse = Parser::new(&input).parse();
+
+        input.clear();
+    }
+}
--- monke_lib/src/lib.rs
+++ monke_lib/src/lib.rs
@@ -1,3 +1,3 @@
+pub mod parser;
 mod lexer;
-mod parser;
 mod syntax;
